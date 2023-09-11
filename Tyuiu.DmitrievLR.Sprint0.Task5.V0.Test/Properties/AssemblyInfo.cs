cb2ba6cd6a5b0684473ab90b0a26e83510ff35d9
DmitrievLeonid <Tyuiu.DmitrievLR@outlook.com>
2023-09-11 15:39:34 +0500

    Добавлен Task5

---
+++ Tyuiu.DmitrievLR.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DmitrievLR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DmitrievLR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51d40d5e-cd27-42c7-9c04-d5c0a6724b8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DmitrievLR.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DmitrievLR.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.DmitrievLR.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сложение
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+
+            //Вычитание
+            Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+
+            //Умножение
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+
+            //Деление с условием if
+            Console.WriteLine("A / b = " + DataService.Division(6,3));
+
+            Console.ReadKey();
+        }
+    }
+}
