51c477c555f048a8eebd715ba855f1fcc9d0f4cf
DmitrievLeonid <Tyuiu.DmitrievLR@outlook.com>
2023-09-13 22:48:24 +0500

    Добавлен Task7

---
+++ Tyuiu.DmitrievLR.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.DmitrievLR.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] ResultArray = new int[5];// так объявляетска кол-во элементов массива 
+
+            for (var i = 0; i < ResultArray.Length; i++)
+            {
+                ResultArray[i] = numOne[i] + numTwo[i]; 
+            }
+
+            return ResultArray;
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DmitrievLR.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DmitrievLR.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0344ba80-1735-45b6-add3-3c964561103f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
